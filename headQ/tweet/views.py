3234aa34bcb4b1b61b32d49361f36945040625f5
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-11-28 01:08:37 +0100

    added edit functionality and solved the bug

--- headQ/tweet/views.py
+++ headQ/tweet/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'index.html')
 
 
-def Tweet_list(request):
+def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request,'tweet_list.html', {'tweets': tweets})
 
@@ -31,12 +31,12 @@ def tweet_create(request):
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id,user =request.user)
     if request.method == 'POST':
-        form = TweetForm(request.POST, request.FILES, isinstance =  tweet)
+        form = TweetForm(request.POST, request.FILES, instance =  tweet)
         if form.is_valid():
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
-            return redirect('Tweet_list')
+            return redirect('tweet_list')
        
     else:
         form = TweetForm(instance=tweet)
